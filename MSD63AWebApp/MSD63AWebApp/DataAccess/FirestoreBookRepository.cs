64e07f50e7e1f34ca54099f4f321179940fb66b9
Ryan Attard <ryanattard@gmail.com>
2023-02-13 09:19:47 +0100

    FireStore Part 1

---
+++ MSD63AWebApp/MSD63AWebApp/DataAccess/FirestoreBookRepository.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.Firestore;
+using MSD63AWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSD63AWebApp.DataAccess
+{
+    public class FirestoreBookRepository
+    {
+        FirestoreDb db;
+        public FirestoreBookRepository(string project)
+        {
+            db = FirestoreDb.Create(project);
+        }
+
+        public void AddBook(Book b)
+        {
+            DocumentReference docRef = db.Collection("books").Document();
+            docRef.SetAsync(b);
+        }
+
+        public async  Task<List<Book>>  GetBooks()
+        {
+            List<Book> books = new List<Book>();
+            Query allBooksQuery = db.Collection("books");
+            QuerySnapshot allBooksQuerySnapshot = await allBooksQuery.GetSnapshotAsync();
+            foreach (DocumentSnapshot documentSnapshot in allBooksQuerySnapshot.Documents)
+            {
+                Book b = documentSnapshot.ConvertTo<Book>();
+                books.Add(b);
+            }
+            return books;
+        }
+    }
+}
---
+++ MSD63AWebApp/MSD63AWebApp/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+
+namespace MSD63AWebApp.Models
+{
+    [FirestoreData] //so that we dont need to store this data formatted as json in Firestore
+    public class Book
+    {
+        [FirestoreProperty]
+        public string Isbn { get; set; }
+        [FirestoreProperty]
+        public string Name { get; set; }
+        [FirestoreProperty]
+        public int Year { get; set; }
+        [FirestoreProperty]
+        public string Author { get; set; }
+        [FirestoreProperty]
+        public string Category { get; set; }
+    }
+}
